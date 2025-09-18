601ae6b74a4f7b1492014dbd0bcf3a47d9797fa6
Wesleynicodemos <wesleyn1373@hotmail.com>
2025-09-18 06:36:58 -0300

    continuando o exercicio 5 de herança e polimorfismo

--- Herança e Polimorfismo/exercicio5/src/application/App.java
+++ Herança e Polimorfismo/exercicio5/src/application/App.java
@@ -1,7 +1,14 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
     }
 }
